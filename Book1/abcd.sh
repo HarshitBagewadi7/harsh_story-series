60afd7a3dce56cdeeeec0465eb446b4a0ae5575f
HarshitBagewadi7 <120817420+HarshitBagewadi7@users.noreply.github.com>
2022-12-18 01:43:07 +0530

    test

---
+++ Book1/abcd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Hello, welcome to Harsh stories!"
+echo " What is your name?"
+read name
+echo "Welcome $name, happy reading!"
+echo "How old are you?"
+read age
+read age_limit
+if [$age -gt $age_limit]
+then
+	echo "You have access to all stories, happy reading!"
+else
+	echo "You have access to kids section, happy reading!"
+fi
