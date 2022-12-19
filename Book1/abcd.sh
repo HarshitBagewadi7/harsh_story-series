5d92f8bab8631f6c1952715ef578326b91e84a5e
HarshitBagewadi7 <120817420+HarshitBagewadi7@users.noreply.github.com>
2022-12-19 10:01:45 +0530

    First_Script_updated

--- Book1/abcd.sh
+++ Book1/abcd.sh
@@ -5,9 +5,7 @@ read name
 echo "Welcome $name, happy reading!"
 echo "How old are you?"
 read age
-echo "Set age limit:"
-read age_limit
-if [ $age -gt $age_limit ]; then
+if [ $age -gt 18 ]; then
 	echo "You have access to all stories, happy reading!"
 else
 	echo "You have access to kids section, happy reading!"
